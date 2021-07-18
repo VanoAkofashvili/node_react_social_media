982588d2408a7f75f8ed8a522679d01501225241
shotius <sh.archemashvili@gmail.com>
2021-07-19 01:28:03 +0400

    removed trash from redux-tk

--- frontend/src/redux_tk/app/store.ts
+++ frontend/src/redux_tk/app/store.ts
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
-import notesReducer from '../features/notes/notesSlice'
 import registerReducer from '../features/register/registerSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    notes: notesReducer,
     register: registerReducer
   },
 })
