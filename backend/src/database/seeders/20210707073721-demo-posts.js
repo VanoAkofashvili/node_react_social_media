dfacd8ebb20bc0505af8735f7d42abcfd92b7215
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-07-22 15:58:33 +0400

    added seeds

--- backend/src/database/seeders/20210707073721-demo-posts.js
+++ backend/src/database/seeders/20210707073721-demo-posts.js
@@ -34,22 +34,18 @@ module.exports = {
     ])
     await queryInterface.bulkInsert('posts', [
       {
-        id: 1,
         content: 'this is the first post',
         itemId: 1
       },
       {
-        id: 2,
         content: 'this is the second post',
         itemId: 2
       },
       {
-        id: 3,
         content: 'this is the third post',
         itemId: 3
       },
       {
-        id: 4,
         content: 'this is the fourth post',
         itemId: 4
       },
@@ -58,41 +54,40 @@ module.exports = {
     await queryInterface.bulkInsert('photos', [
       {
         id: 1,
-        path: 'images/first.jpg',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        id: 2,
-        path: 'images/second.jpg',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        id: 3,
-        path: 'images/third.jpg',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        id: 4,
-        path: 'images/fourth.jpg',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ])
-
-    await queryInterface.bulkInsert('user_photos', [
-      {
-        photoId: 1,
         postId: 1,
-      }, {
-        photoId: 2,
-        postId: 2,
-      }, {
-        photoId: 3,
-        postId: 3,
-      }, {
-        photoId: 4,
-        postId: 4,
+        path: 'images/7f651e58-9b1e-4108-9a6e-ea8e778c3c5f.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
+      {
+        id: 2,
+        postId: 2,
+        path: 'images/15a7e380-602e-49b7-8b26-ad15ec685f8c.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 3,
+        postId: 1,
+        path: 'images/5fd0ca2d-e573-42ea-9402-264c541dff4f.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 4,
+        postId: 1,
+        path: 'images/9e3df341-e423-4c2a-a381-f3ae029efc4b.png',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 5,
+        postId: 2,
+        path: 'images/3c66d5c4-641f-4f88-8567-5d92b762b5d2.png',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+
     ])
   },
 
@@ -100,6 +95,5 @@ module.exports = {
     await queryInterface.bulkDelete("items", null, {});
     await queryInterface.bulkDelete("posts", null, {});
     await queryInterface.bulkDelete("photos", null, {});
-    await queryInterface.bulkDelete("user_photos", null, {});
   }
 };
